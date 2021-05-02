cb6fa4fcf504bd5f80c01f2f0a288ba1f8ab6174
binarySigh <951382408@qq.com>
2021-05-02 09:39:36 +0800

    LC Commit

--- src/main/java/com/mjscode/algorithms/leetcode/LC0090_SubsetWithDup.java
+++ src/main/java/com/mjscode/algorithms/leetcode/LC0090_SubsetWithDup.java
@@ -1,9 +1,6 @@
 package com.mjscode.algorithms.leetcode;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 /**
  * //给你一个整数数组 nums ，其中可能包含重复元素，请你返回该数组所有可能的子集（幂集）。
@@ -32,11 +29,62 @@ import java.util.PriorityQueue;
 public class LC0090_SubsetWithDup {
 
     public static void main(String[] args){
+        //int[] nums = {1,2,2};
+        //int[] nums = {0};
 
+        // 期待结果：[[],[1],[1,2],[1,2,3],[1,3],[2],[2,3],[3]]
+        //int[] nums = {1,2,3};
+
+        //期望结果: [[],[1],[1,4],[1,4,4],[1,4,4,4],[1,4,4,4,4],[4],[4,4],[4,4,4],[4,4,4,4]]
+        int[] nums = {4,4,4,1,4};
+        List<List<Integer>> list = subsetsWithDup(nums);
+        System.out.println("[[],[1],[1,4],[1,4,4],[1,4,4,4],[1,4,4,4,4],[4],[4,4],[4,4,4],[4,4,4,4]]");
+        System.out.println(list);
     }
 
-    /*public static List<List<Integer>> subsetsWithDup(int[] nums) {
+    /**
+     * 本题说是要求子集，其实是他妈的子序列
+     * 而且还尼玛要先排序
+     *
+     * 解答成功:
+     * 		执行耗时:3 ms,击败了32.57% 的Java用户
+     * 		内存消耗:38.9 MB,击败了34.65% 的Java用户
+     * @param nums
+     * @return
+     */
+    public static List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        if(nums.length < 1){
+            return list;
+        }
+        Arrays.sort(nums);
+        fillList(nums, list, new ArrayList<>(),0, new HashSet<>());
+        return list;
+    }
 
-    }*/
+    public static void fillList(int[] nums, List<List<Integer>> list, List<Integer> cur, int index, HashSet<String> set){
+        if(index == nums.length){
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < cur.size(); i++){
+                sb.append(cur.get(i)).append('-');
+            }
+            if(!set.contains(sb.toString())){
+                set.add(sb.toString());
+                list.add(cur);
+            }
+            return;
+        }
+        List<Integer> tmp = new ArrayList<>();
+        for(int i = 0; i < cur.size(); i++){
+            tmp.add(cur.get(i));
+        }
+        fillList(nums, list, tmp,index + 1, set);
+        tmp = new ArrayList<>();
+        for(int i = 0; i < cur.size(); i++){
+            tmp.add(cur.get(i));
+        }
+        tmp.add(nums[index]);
+        fillList(nums, list, tmp,index + 1, set);
+    }
 
 }
